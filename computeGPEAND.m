dcff65d800def65a00b081c2839554bb6701c610
Alfredo Esquivel Jaramillo <50448917+alfredoesquivel1987@users.noreply.github.com>
2020-05-19 21:48:22 +0200

    Update computeGPEAND.m

--- computeGPEAND.m
+++ computeGPEAND.m
@@ -1,13 +1,8 @@
 function [GPE,scaledGPE]=computeGPEAND(trueValues,estimatedValues, percent,N)
-    % find the indices where both signals are free of NaNs
-    %%See Matlab Mathworks definition: 
-    %%https://se.mathworks.com/help/audio/ref/pitch.html
+
   nanIndicator = ~(isnan(trueValues)) & ~(isnan(estimatedValues));
-   %nanIndicator = ~(isnan(trueValues) & isnan(estimatedValues));
-    %nanIndicator = ~isnan(trueValues); 
-    scaledGPE= length(find(abs(estimatedValues(nanIndicator)-...
+  scaledGPE= length(find(abs(estimatedValues(nanIndicator)-...
         trueValues(nanIndicator))>trueValues(nanIndicator)*percent))/N*100; 
     GPE = mean(abs(estimatedValues(nanIndicator)-...
         trueValues(nanIndicator))>trueValues(nanIndicator)*percent)*100; %
-    %%%%%%%%scaledGPE = proportion*GPE; 
-end
+  end
